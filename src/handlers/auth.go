cdbd743a344e1d8303b5d018024072ba5ad69be9
smorodin-it <smorodin.it@gmail.com>
2023-12-09 20:20:05 +0300

    generate tokens

--- src/handlers/auth.go
+++ src/handlers/auth.go
@@ -5,7 +5,10 @@ import (
 	"food-backend/src/forms"
 	"food-backend/src/repositories"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
+	"time"
 )
 
 func HandleAuth(ctx *fiber.Ctx) error {
@@ -34,11 +37,27 @@ func HandleAuth(ctx *fiber.Ctx) error {
 		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
 	}
 
-	return ctx.Status(fiber.StatusOK).JSON(user)
-
 	// Generate tokens
+
+	token := jwt.New(jwt.SigningMethodHS512)
+
+	claims := token.Claims.(jwt.MapClaims)
+	claims["id"] = user.Id
+	claims["email"] = user.Email
+
+	exp := time.Now().Add(time.Minute)
+
+	claims["exp"] = exp.Unix()
+
+	t, err := token.SignedString([]byte("3be9067a-0869-4291-ae46-1e943f05642a"))
+	if err != nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+	}
 
 	// Return tokens
 
-	//return nil
+	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
+		"token":        t,
+		"refreshToken": uuid.New().String(),
+	})
 }
