e4f0e5b5e23f79ee75e82eb43f5024590b25eb23
smorodin-it <smorodin.it@gmail.com>
2023-12-16 14:50:03 +0300

    dev

--- src/domains/user.go
+++ src/domains/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	Id           string    `json:"id"  db:"id"`
 	Email        string    `json:"email" db:"email"`
-	RefreshToken *it string   `json:"-" db:"refresh_token"`
+	RefreshToken *string   `json:"-" db:"refresh_token"`
 	PasswordHash string    `json:"-" db:"password_hash"`
 	CreatedAt    time.Time `json:"-" db:"created_at"`
 	UpdatedAt    time.Time `json:"-" db:"updated_at"`
